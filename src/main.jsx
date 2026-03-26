0f97ec0d8d22df50f2e8e626ebca6ee41c3b4d5a
Mario <mario.munoz@nfq.es>
2026-03-26 09:44:53 +0100

    Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './app.jsx'
 import './styles/index.css'
 
+// 1. Importar la librería de Google Analytics
+import ReactGA from "react-ga4";
+
+// 2. Inicializar con tu ID de medición
+ReactGA.initialize("G-YBSZ84J0Q1");
+
+// 3. Registrar la primera visita nada más cargar la app
+ReactGA.send({ hitType: "pageview", page: window.location.pathname, title: "Inicio In Essence" });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
